bedc98be9c51ca3b96cc53dc052f33463e0a313e
AKA MAX <kalimeelan977@gmail.com>
2024-08-01 04:04:35 +0545

    Create run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+
+import os,sys
+print(' • Checking updates');os.system('git pull -q');print('')
+bit = platform.architecture()[0]
+if bit == '32bit':
+    print(" • This tool doesn't support 32bit devices");exit()
+elif bit == '64bit':
+    import MFBC64
+else: print(" Your device doesn't support this tool ");exit()
